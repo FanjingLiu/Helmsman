7017f6366034b8a525dfdce7763ec423882b4a5f
Tank <geckoliu93@gmail.com>
2019-03-05 04:05:57 -0600

    .

--- land/urls.py
+++ land/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 app_name = "land"
 urlpatterns = [
-    path("/",views.homepage, name="homepage"),
+    path("",views.homepage, name="homepage"),
     path("",views.homepage, name="homepage")
 ]
